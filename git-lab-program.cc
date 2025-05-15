34648730a9b12d53d5d5063c794dd2dc56b3c5ce
shockjohnson0 <mj934122@ohio.edu>
2025-05-15 17:30:07 -0400

    Initial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Shock Johnson
+ *        Date: 05/15/2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello, World!" << endl;
+    return 0;
+}// main
